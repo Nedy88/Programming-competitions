c65058f47de786702736fd124e18353b9a0c8d89
Nedyalko Prisadnikov <nedy88@gmail.com>
2011-07-24 19:06:20 +0300

    Wrongly added files

--- 1/1a.py
+++
@@ -1,8 +0,0 @@
-width, height, size = map(int, raw_input().split())
-horizontal = width / size
-if width % size != 0:
-    horizontal += 1
-vertical = height / size
-if height % size != 0:
-    vertical += 1
-print horizontal * vertical
